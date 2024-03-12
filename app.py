d7c5859f8486520faff1afcb8c3744f0b4382fc6
Josiah Randleman <69489368+jrandl@users.noreply.github.com>
2024-03-11 21:57:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,12 +7,39 @@ import palmerpenguins  # This package provides the Palmer Penguins dataset
 penguins_df = palmerpenguins.load_penguins()
 
 ui.page_opts(title="Palmer Penguin's Data: Josiah Randleman", fillable=True)
-with ui.layout_columns():
 
-    @render_plotly
-    def plot1():
-        return px.histogram(px.data.tips(), y="tip")
+with ui.sidebar(bg="#f8f8f8", open="open"):  
+    ui.h2("Sidebar")
+    
+    ui.input_selectize(
+        "selected_attribute",  # Unique identifier for the input
+        "Choose A Selected Attribute:",  # Label for the input
+        ["bill_length_mm", "bill_depth_mm", "flipper_length_mm", "body_mass_g"],
+        multiple=False  # Allow multiple selections
+    )
 
-    @render_plotly
-    def plot2():
-        return px.histogram(px.data.tips(), y="total_bill")
+    ui.input_numeric("plotly_bin_count", "Plotly Histogram Bins:", 10)
+
+    ui.input_slider("seaborn_bin_count", "Seaborn Bins:", min=0, max=100, value=25)
+
+    ui.input_checkbox_group(
+        "selected_species_list",
+        "Choose Species:",
+        ["Adelie", "Gentoo", "Chinstrap"],
+        selected= ["Adelie"],
+        inline=False,
+    )
+
+    ui.hr()
+
+    ui.a("GitHub", href="https://github.com/jrandl/cintel-02-data", target="_blank")
+
+#with ui.layout_columns():
+#
+#    @render_plotly
+#    def plot1():
+#        return px.histogram(px.data.tips(), y="tip")
+#
+#    @render_plotly
+#    def plot2():
+#        return px.histogram(px.data.tips(), y="total_bill")
